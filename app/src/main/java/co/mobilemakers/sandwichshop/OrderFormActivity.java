37896ff9356426f9bf5987f1f6f3ec23dcb275d8
Agustín <agustin.gugliotta@globant.com>
2015-01-26 17:05:57 -0300

    Added logic of the place order's button

--- app/src/main/java/co/mobilemakers/sandwichshop/OrderFormActivity.java
+++ app/src/main/java/co/mobilemakers/sandwichshop/OrderFormActivity.java
@@ -1,17 +1,32 @@
 package co.mobilemakers.sandwichshop;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class OrderFormActivity extends ActionBarActivity {
+
+    Button mButtonPlaceOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonPlaceOrder = (Button) findViewById(R.id.button_place_order);
+
+        mButtonPlaceOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OrderFormActivity.this, OrderConfirmationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
